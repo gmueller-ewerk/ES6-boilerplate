59a4b1553b702727d4cc7898f1c3bd046aa8a075
Gerd Müller <g.mueller@ewerk.com>
2017-09-25 09:49:33 +0200

    Update indes files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-const sayHello = () => {
-    /*eslint-disable no-console */
-    console.log("Allo! We are all set!");
-    console.log("Arrow functions are working");
+var calculateMonthlyPayment = function (principal, years, rate) {
+    if (rate) {
+        var monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    return monthlyPayment;
 };
 
-sayHello();
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+});
